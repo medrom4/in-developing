cc19f88581c4952d7155ab3796df73f3a7fdda80
Ramanovich <medikroma@gmail.com>
2019-01-22 20:42:41 +0000

    function-bind

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,15 +1,23 @@
-function check(args) {
-	var actual = args.length;
-	var expected = args.callee.length;
-	if (actual !== expected)
-		throw new Error("ожидается: " + expected + "; получено: " + actual);
-}
+var sum = function (x, y) {
+	return x + y
+};
+var succ = sum.bind(null, 1);
+succ(2)
 
-function f(x, y, z) {
-	check(arguments);
-	return x + y + z;
-}
+console.log(succ(2));
+
+function f(y, z) {
+	return this.x + y + z
+};
+
+var g = f.bind({
+	x: 1
+}, 2);
+g(3)
+
+console.log(g(3));
 
 
-//call() - методы, выполняющий косвенный вызов функции
-//apply() - методы, выполняющий косвенный вызов функции
+//a.call() - методы, выполняющий косвенный вызов функции
+//a.apply() - методы, выполняющий косвенный вызов функции
+//a.bind() - связать (bind) функцию с объектом
