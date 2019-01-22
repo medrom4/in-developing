695a36c89f80156b12e8dbbe0d87b4a305bf87f0
Ramanovich <medikroma@gmail.com>
2019-01-22 18:34:29 +0000

    function-anonim-extend()

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,8 +1,33 @@
-uniqueInteger.counter = 0;
+var extend = (function () {
 
-function uniqueInteger() {
-	return uniqueInteger.counter++;
-}
+	for (var p in {
+			toString: null
+		}) {
+		return function extend(o) {
+			for (var i = 1; i < arguments.length; i++) {
+				var source = arguments[i];
+				for (var prop in source) o[prop] = source[prop];
+			}
+			return o;
+		};
+	}
+
+
+	var protoprops = ["toString", "valueOf", "constructor", "hasOwnProperty",
+"isPrototypeOf", "propertyIsEnumerable", "toLocaleString"];
+	return function patched_extend(o) {
+		for (var i = 1; i < arguments.length; i++) {
+			var source = arguments[i];
+			for (var prop in source) o[prop] = source[prop];
+			for (var j = 0; j < protoprops.length; j++) {
+				prop = protoprops[j];
+				if (source.hasOwnProperty(prop)) o[prop] = source[prop];
+			}
+		}
+		return o;
+	};
+}());
+
 
 
 
