4f8cfd4962fbd085627ba47c0038e18bba07f891
Ramanovich <medikroma@gmail.com>
2019-01-27 14:34:10 +0000

    OOP-JS-enum

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,88 +1,17 @@
-function Set() {
-    this.value = {};
-    this.n = 0;
-    this.add.apply(this, arguments);
-}
+var Coin = ({
+    Penny: 1,
+    Nickel: 5,
+    Dime: 10,
+    Quarter: 25
+});
+var c = Coin.Dime;
 
-//Добавляет все аргументы в множество.
-Set.prototype.add = function () {
-    for (var i = 0; i < arguments.length; i++) {
-        var val = arguments[i];
-        var str = Set._v2s(val);
-        if (!this.values.hasOwnProperty(str)) {
-            this.values[str] = val;
-            this.n++;
-        }
-    }
-    return this;
-}
-
-
-//Удаляет все аргументы из множества.
-Set.prototype.remove = function () {
-    for (var i = 0; i < arguments.length; i++) {
-        var str = Set._v2s(arguments[i]);
-        if (this.values.hasOwnProperty(str)) {
-            delete this.values[str];
-            this.n--;
-        }
-    }
-    return this;
-}
-
-//Возвращает true, если множество содержит value; иначе возвращает false.
-Set.prototype.contains = function (value) {
-    return this.values.hasOwnProperty(Set._v2s(value));
-};
-
-
-// Возвращает размер множества
-Set.prototype.size = function () {
-    return this.n;
-};
-
-
-Set.prototype.foreach = function (f, context) {
-    for (var s in this.values)
-        if (this.values.hasOwnProperty(s))
-            f.call(context, this.values[s]);
-}
-
-
-Set._v2s = function (val) {
-    switch (val) {
-        case undefined:
-            return 'u';
-        case null:
-            return 'n';
-        case true:
-            return 't';
-        case false:
-            return 'f';
-        default:
-            switch (typeof val) {
-                case 'number':
-                    return '#' + val;
-                case 'string':
-                    return '"' + val;
-                default:
-                    return '@' + objectId(val);
-            }
-    }
-
-    function objectId(o) {
-        var prop = "|**objectid**|";
-        if (!o.hasOwnProperty(prop))
-            o[prop] = Set._v2s.next++;
-        return o[prop];
-    }
-
-};
-
-Set._v2s.next = 100;
-
-
-
+console.log(c instanceof Coin);
+console.log(c.constructor == Coin);
+console.log(Coin.Quarter + 3 * Coin.Nickel);
+console.log(Coin.Dime == 10);
+console.log(Coin.Dime > Coin.Nickel);
+console.log(String(Coin.Dime) + ":" + Coin.Dime);
 
 
 // a.call() - методы, выполняющий косвенный вызов функции
