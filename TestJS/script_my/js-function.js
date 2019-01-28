1e2f806364c3dad0b4606246275ff0f7a0ce513d
Ramanovich <medikroma@gmail.com>
2019-01-28 19:21:09 +0000

    regular-expressions

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,17 +1,38 @@
-var Coin = ({
-    Penny: 1,
-    Nickel: 5,
-    Dime: 10,
-    Quarter: 25
-});
-var c = Coin.Dime;
+var parent = /s$/;
 
-console.log(c instanceof Coin);
-console.log(c.constructor == Coin);
-console.log(Coin.Quarter + 3 * Coin.Nickel);
-console.log(Coin.Dime == 10);
-console.log(Coin.Dime > Coin.Nickel);
-console.log(String(Coin.Dime) + ":" + Coin.Dime);
+console.log(parent);
+
+
+
+
+
+
+
+
+
+// регулярные выражения
+//   /s$/  - ищет любые строки, заканчивающиеся символом «s».
+//   \n - перевод строки
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+//   
+
+
+
 
 
 // a.call() - методы, выполняющий косвенный вызов функции
