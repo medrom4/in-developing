34ec0020836febb778fcdaac8b86b6eeabaef42d
Ramanovich <medikroma@gmail.com>
2019-02-04 20:01:00 +0000

    timer

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,19 +1,11 @@
-window.onload = function () {
-	var elements = document.getElementsByClassName('reveal');
-	for (var i = 0; i < elements.length; i++) {
-		var elt = elements[i];
-		var title = elt.getElementsByClassName('handle')[0];
-		addRevealHandler(title, elt);
-	}
+function displayTime() {
+	var elt = document.getElementById('clock');
+	var now = new Date();
+	elt.innerHTML = now.toLocaleTimeString();
+	setTimeout(displayTime, 1000);
+}
 
-	function addRevealHandler(title, elt) {
-		title.onclick = function () {
-			if (elt.className == 'reveal') elt.className = 'revealed';
-			else if (elt.className == 'revealed') elt.className = 'reveal';
-		}
-	}
-};
-
+window.onload = displayTime;
 
 
 
