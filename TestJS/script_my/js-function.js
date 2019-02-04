735908d971900925e0fb7dc4f56d237e1fd9d166
Ramanovich <medikroma@gmail.com>
2019-02-04 21:09:31 +0000

    onLoad-onload

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,11 +1,13 @@
-function factorial(n) {
-	if (n <= 1) return n;
-	else return n * factorial(n - 1);
+function onLoad(f) {
+    if (onLoad.loaded)
+        window.setTimeout(f, 0);
+    else if (window.addEventListener)
+        window.addEventListener("load", f, false);
+    else if (window.attachEvent)
+        window.attachEvent("onload", f);
 }
-document.write("<table>");
-document.write("<tr><th>n</th><th>n!</th></tr>");
-for (var i = 1; i <= 10; i++) {
-	document.write("<tr><td>" + i + "</td><td>" + factorial(i) + "</td></tr>");
-}
-document.write("</table>");
-document.write("Произведено вычисление: " + new Date());
+onLoad.loaded = false;
+
+onLoad(function() {
+    onLoad.loaded = true;
+});
