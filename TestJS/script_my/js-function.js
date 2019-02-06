e62c67bffc5dbfc4f8d375917227d49ba05dac31
Ramanovich <medikroma@gmail.com>
2019-02-06 21:16:37 +0000

    dialog

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,6 +1,6 @@
-(function() {
-	var a = window.screen;
-	
-	console.log(a);
-})();
+do {
+    var name = prompt('Введите Ваше имя!');
+    var correct = confirm("Вы ввели ' " + name + " '. \n" + "Щелкните ОК, чтобы продолжить, " + "или Отмена, чтобы повторить ввод.")
+} while (!correct)
 
+alert("Привет, " + name);
