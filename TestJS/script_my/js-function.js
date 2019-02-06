652f3b53f147fcc2b7c9ba29bc84b1c285018720
Ramanovich <medikroma@gmail.com>
2019-02-06 21:06:19 +0000

    window-screen

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,18 +1,6 @@
-var args = urlArgs();
-var q = args.q || "";
-var n = args.n ? parseInt(args.n) : 10;
+(function() {
+	var a = window.screen;
+	
+	console.log(a);
+})();
 
-function urlArgs() {
-    var args = {};
-    var query = location.search.substring(1);
-    var pairs = query.split("&");
-    for (var i = 0; i < pairs.length; i++) {
-        var pos = pairs[i].indexOf('=');
-        if (pos == -1) continue;
-        var name = pairs[i].substring(0, pos);
-        var value = pairs[i].substring(pos + 1);
-        value = decodeURIComponent(value);
-        args[name] = value;
-    }
-    return args;
-}
