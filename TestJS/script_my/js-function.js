7bc80a6b2e72db6c311d9e06ba81e44fb86c8398
Ramanovich <medikroma@gmail.com>
2019-02-06 20:44:17 +0000

    js-URL-search

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,15 +1,18 @@
-function invoke(f, start, interval, end) {
-    if (!start) start = 0;
-    if (arguments.length <= 2)
-        setTimeout(f, start);
-    else {
-        setTimeout(repeat, start);
+var args = urlArgs();
+var q = args.q || "";
+var n = args.n ? parseInt(args.n) : 10;
 
-        function repeat() {
-            var h = setInterval(f, interval);
-            if (end) setTimeout(function() {
-                clearInterval(h);
-            }, end);
-        }
+function urlArgs() {
+    var args = {};
+    var query = location.search.substring(1);
+    var pairs = query.split("&");
+    for (var i = 0; i < pairs.length; i++) {
+        var pos = pairs[i].indexOf('=');
+        if (pos == -1) continue;
+        var name = pairs[i].substring(0, pos);
+        var value = pairs[i].substring(pos + 1);
+        value = decodeURIComponent(value);
+        args[name] = value;
     }
+    return args;
 }
