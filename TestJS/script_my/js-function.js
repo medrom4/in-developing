fce1f36d7f05edd3faa29d269a72f0f29cdbeb46
Ramanovich <medikroma@gmail.com>
2019-02-08 20:37:59 +0000

    image_src

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,21 +1,4 @@
-(function() {
-    function sibling(e, n) {
-        while (e && n !== 0) {
-            if (n > 0) {
-                if (e.nextElementSibling) e = e.nextElementSibling;
-                else {
-                    for (e = e.nextSibling; e && e.nodeType !== 1; e = e.nextSibling)
-                }
-                n--;
-            }
-            else {
-                if (e.previousElementSibing) e = e.previousElementSibling;
-                else {
-                    for (e = e.previousSibling; e && e.nodeType !== 1; e = e.previousSibling)
-                }
-                n++;
-            }
-        }
-        return e;
-    }
-})();
+var image = document.getElementById("myimage");
+var imgurl = image.src;
+
+console.log(imgurl);
