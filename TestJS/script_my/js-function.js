05f3d808d006c51d4e700f3c0abc18a5121589e6
Ramanovich <medikroma@gmail.com>
2019-02-08 20:31:38 +0000

    nextElementSibling

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,5 +1,21 @@
-(function () {
-	var a = document.getElementsByTagName("a");
-	for (var i = 0; i < a.length; i++)
-		a[i].style.display = "none";
+(function() {
+    function sibling(e, n) {
+        while (e && n !== 0) {
+            if (n > 0) {
+                if (e.nextElementSibling) e = e.nextElementSibling;
+                else {
+                    for (e = e.nextSibling; e && e.nodeType !== 1; e = e.nextSibling)
+                }
+                n--;
+            }
+            else {
+                if (e.previousElementSibing) e = e.previousElementSibling;
+                else {
+                    for (e = e.previousSibling; e && e.nodeType !== 1; e = e.previousSibling)
+                }
+                n++;
+            }
+        }
+        return e;
+    }
 })();
