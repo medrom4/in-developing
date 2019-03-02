5a39a0473d5e0e8f6b4c58b436bb71c256a5fa79
Ramanovich <medikroma@gmail.com>
2019-03-02 19:04:13 +0000

    add-style-css

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,4 +1,22 @@
+// меняет значение fontSize
 function scale(e, factor) {
 	var size = parseInt(window.getComputedStyle(e, "").fontSize);
 	e.style.fontSize = factor * sise + "px";
+}
+
+// меняет значение цвета
+
+function scaleColor(e, factor) {
+	var color = window.getComputedStyle(e, "").backgroundColor;
+	var components = color.match(/[\d\.]+/g);
+	for (var i = 0; i < 3; i++) {
+		var x = Number(components[i]) * factor;
+		x = Math.round(Math.min(Math.max(x, 0), 255));
+		components[i] = String(x)
+	}
+
+	if (components.length == 3)
+		e.style.backgroundColor = "rgb(" + components.join() + ")";
+	else
+		e.style.backgroundColor = "rgba(" + components.join() + ")";
 }
