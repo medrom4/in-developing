9692d2e05e91379583ecbf553799f058373c50eb
Ramanovich <medikroma@gmail.com>
2019-03-04 20:29:37 +0000

    newfunctionAddEventListener

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,9 +1,3 @@
-var b = document.getElementById("mybutton");
-
-b.onclick = function() {
-    alert("Спасибо, я запомнил.");
-};
-
-b.addEventListener("click", function() {
-    alert("Ещё раз выскачило");
-}, false);
+window.onload = onnn = () => {
+    alert("загрузка строаницы завершена!");
+}
