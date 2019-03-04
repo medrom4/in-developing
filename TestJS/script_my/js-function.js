9623d6258c7cf754192200482341ccca45f51763
Ramanovich <medikroma@gmail.com>
2019-03-04 14:51:26 +0000

    addEventLi

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,26 +1,6 @@
-function addStyles(styles) {
-    var styleElt, styleSheet;
-    if (document.createStyleSheet) {
-        styleSheet = document.createStyleSheet();
-    } else {
-        var head = document.getElementsByTagName("head")[0]
-        styleElt = document.createElement("style");
-        head.appendChild(styleElt);
-        styleSheet = document.styleSheets[document.styleSheets.length - 1]
-    }
-
-    if (typeof styles === "string") {
-        if (styleElt) styleElt.innerHTML = styles;
-        else styleSheet.cssText = styles;
-    } else {
-        var i = 0;
-        for (selector in styles) {
-            if (styleSheet.insertRule) {
-                var rule = selector + " {" + styles[selector] + "}";
-                styleSheet.insertRule(rule, i++);
-            } else {
-                styleSheet.addRule(selector, styles[selector], i++);
-            }
-        }
+window.onload = function () {
+    var elt = document.getElementById("shipping_addres");
+    elt.onsubmit = function () {
+        return validate(this);
     }
 }
