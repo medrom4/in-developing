70e8e8ceb270674891879ca649dd3be8dcbb5a33
Ramanovich <medikroma@gmail.com>
2019-03-04 21:20:30 +0000

    drag

--- TestJS/script_my/js-function.js
+++ TestJS/script_my/js-function.js
@@ -1,33 +1,69 @@
-var whenReady = (function() {
-    var funcs = [];
-    var ready = false;
+function getScrollOffsets(w) {
+    w = w || window;
+    if (w.pageXOffset != null) return { x: w.pageXOffset, y: w.pageYOffset };
+    var d = w.document;
+    if (document.compatMode == "CSS1Compat")
+        return {
+            x: d.documentElement.scrollLeft,
+            y: d.documentElement.scrollTop
+        };
+    return { x: d.body.scrollLeft, y: d.body.scrollTop };
+}
 
-    function handler(e) {
-        if (ready) return;
+function drag(elementToDrag, event) {
+    var scroll = getScrollOffsets();
 
-        if (e.type === "readystatechange" && document.readyState !== "complete")
-            return;
+    var startX = event.clientX + scroll.x;
+    var startY = event.clientY + scroll.y;
 
-        for (var i = 0; i < funcs.length; i++)
-            funcs[i].call(document);
+    var origX = elementToDrag.offsetLeft;
+    var origY = elementToDrag.offsetTop;
 
-        ready = true;
-        funcs = null;
-    }
+    var deltaX = startX - origX;
+    var deltaY = startY - origY;
 
     if (document.addEventListener) {
-        document.addEventListener("DOMContentLoaded", handler, false);
-        document.addEventListener("readystatechange", handler, false);
-        window.addEventListener("load", handler, false);
+        document.addEventListener("mousemove", moveHandler, true);
+        document.addEventListener("mouseup", upHandler, true);
     }
 
     else if (document.attachEvent) {
-        document.attachEvent("onreadystatechange", handler);
-        window.attachEvent("onload", handler);
+        elementToDrag.setCapture();
+        elementToDrag.attachEvent("onmousemove", moveHandler);
+        elementToDrag.attachEvent("onmouseup", upHandler);
+        elementToDrag.attachEvent("onlosecapture", upHandler);
     }
 
-    return function whenReady(f) {
-        if (ready) f.call(document);
-        else funcs.push(f);
+    if (event.stopPropagation) event.stopPropagation();
+    else event.cancelBubble = true;
+    if (event.preventDefault) event.preventDefault();
+    else event.returnValue = false;
+
+    function moveHandler(e) {
+        if (!e) e = window.event;
+        var scroll = getScrollOffsets();
+        elementToDrag.style.left = (e.clientX + scroll.x - deltaX) + "px";
+        elementToDrag.style.top = (e.clientY + scroll.y - deltaY) + "px";
+
+        if (e.stopPropagation) e.stopPropagation();
+        else e.cancelBubble = true;
     }
-}());
+
+    function upHandler(e) {
+        if (!e) e = window.event;
+        if (document.removeEventListener) {
+            document.removeEventListener("mouseup", upHandler, true);
+            document.removeEventListener("mousemove", moveHandler, true);
+        }
+
+        else if (document.detachEvent) {
+            elementToDrag.detachEvent("onlosecapture", upHandler);
+            elementToDrag.detachEvent("onmouseup", upHandler);
+            elementToDrag.detachEvent("onmousemove", moveHandler);
+            elementToDrag.releaseCapture();
+        }
+
+        if (e.stopPropagation) e.stopPropagation();
+        else e.cancelBubble = true;
+    }
+}
