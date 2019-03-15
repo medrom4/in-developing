b56a09a814808909e08d8100bd50c534b2ec3696
Ramanovich <medikroma@gmail.com>
2019-03-15 20:44:27 +0000

    HTTP-POST

--- TestJS/script_my/XMLHttpRequest.js
+++ TestJS/script_my/XMLHttpRequest.js
@@ -124,3 +124,25 @@ function postQuery(url, what, where, radius, callback) {
 
     request.send(doc);
 }
+
+
+// Выгрузка файла посредством запроса HTTP POST
+
+(function() {
+    var elts = document.getElementsByTagName("input");
+    for (var i = 0; i < elts.length; i++) {
+        var input = elts[i];
+        if (input.type !== "file") continue;
+
+        var url = input.getAttribute("data-uploadto");
+        if (!url) continue;
+
+        input.addEventListener("change", function() {
+            var file = this.files[0];
+            if (!file) return;
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url);
+            xhr.send(file);
+        }, false);
+    }
+})();
