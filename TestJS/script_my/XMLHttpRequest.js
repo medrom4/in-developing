a71c3b335aeae052e55492e957df4b1f1a05afc6
Ramanovich <medikroma@gmail.com>
2019-03-15 22:18:29 +0000

    timedout

--- TestJS/script_my/XMLHttpRequest.js
+++ TestJS/script_my/XMLHttpRequest.js
@@ -259,3 +259,25 @@ function postFormData(url, data, callback) {
     }
 
 })();
+
+// Реализация поддержки предельного времени ожидания
+
+function timedGetText(url, timeout, callback) {
+    var request = new XMLHttpRequest();
+    var timedout = false;
+
+    var timer = setTimeout(function() {
+        timedout = true;
+        request.abort();
+    }, timeout);
+
+    request.open("GET", url);
+    request.onreadystatechange = function() {
+        if (request.readyState !== 4) return;
+        if (timedout) return;
+        clearTimeout(timer);
+        if (request.status === 200)
+            callback(request.responseText);
+    };
+    request.send(null);
+}
