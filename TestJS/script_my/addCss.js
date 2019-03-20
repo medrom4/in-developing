ade48245acb8aee3357904956383f696087d096c
Ramanovich <medikroma@gmail.com>
2019-03-20 20:15:15 +0000

    text()

--- TestJS/script_my/addCss.js
+++ TestJS/script_my/addCss.js
@@ -1,3 +1,21 @@
 (function() {
     $("div").addClass("hilite");
 })();
+
+
+(function() {
+    var title = $("head title").text();
+    var hedline = $("body h1").text();
+    
+    $("h1").text(function(n, current) {
+        return "$" + (n + 1) + ": " + current  
+    });
+    
+    console.log(title);
+    console.log();
+    
+    console.log(hedline);
+    console.log();
+    
+    
+})();
