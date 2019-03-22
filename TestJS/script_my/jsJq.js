805cb2056a34524079ada8e06cc88d7c576f5d84
Ramanovich <medikroma@gmail.com>
2019-03-22 21:03:48 +0000

    jQuery-ajax

--- TestJS/script_my/jsJq.js
+++ TestJS/script_my/jsJq.js
@@ -28,8 +28,71 @@
 
 
 
+// (function() {
+//     $("#imgTop").fadeIn(1800);
+//     $("#imgTop").fadeOut(1800);
+// })();
 
 
 
-// bind()
-// rigger()
+// (function() {
+//     $("#imgTop2").fadeOut(1800).show(1800).slideUp(1800).slideToggle(1800);
+// })();
+
+
+
+// $("h1").animate({
+//     opacity: .25,
+//     fontSize: 10
+// }, {
+//     duration: 2500,
+//     complete: function() {
+//         this.text("До свидания");
+//     }
+// });
+
+
+// $("#imgTop2").animate({ "width": "+=500" }, 500, "linear");
+
+$("#imgTop2").bind({
+    mouseover: function() { $(this).stop().fadeTo(300, 1.0); },
+    mouseout: function() { $(this).stop().fadeTo(300, 0.5); }
+});
+
+
+
+
+$("#message").fadeIn().delay(200).queue(function(next) {
+            $(this).text("Привет, Мир!");
+                next();
+            }).animate({ borderWidth: "+=10px;" });
+
+
+
+
+
+
+
+
+
+
+        // bind()
+        // rigger()
+        // delegate() и undelegate()
+
+
+        // АНИМАЦИОННЫЕ ЭФФЕКТЫ
+        // fadeIn(), fadeOut(), fadeTo()
+        // show(), hide(), toggle()
+        // slideDown(), slideUp(скрывает выбранные элементы в объекте jQuery, постепенно уменьшая их высоту до 0), slideToggle(выполняет противоположные действия)
+        // 
+        //  
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        //
