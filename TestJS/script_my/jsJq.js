a696a6a12c74added1af946b3235ad5fbd658795
Ramanovich <medikroma@gmail.com>
2019-03-22 20:20:04 +0000

    jQuery-unbind

--- TestJS/script_my/jsJq.js
+++ TestJS/script_my/jsJq.js
@@ -3,3 +3,15 @@
         $(this).css("background-color", "gray");
     });
 })();
+
+
+
+(function() {
+    $("img").click(function() {
+        $(this).css("opacity", "30%");
+    });
+})();
+
+
+// Удалить все обработчики событий mouseover и mouseout во всех элементах <a>
+$('a').unbind("mouseover mouseout");
