e38cf2f90fadc711ec9ce945e29602a76ad04988
Ramanovich <medikroma@gmail.com>
2019-03-25 21:21:17 +0000

    ajax-load-get

--- TestJS/script_my/jsAjax.js
+++ TestJS/script_my/jsAjax.js
@@ -1,3 +1,8 @@
-(function() {
-    $("#stats").load("status_report.html");
-}, 60000);
+// (function() {
+//     $("#stats").load("status_report.html");
+// }, 60000);
+
+
+(function(){
+    $('#stats').load("weather.com/weather/today/l/02134:4:US", "zipcode=02134");
+})();
